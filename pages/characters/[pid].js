c2a62a4bab318144c56ca7ddb9d3f1b244ffdc0a
Nick Lontorfos <nlontorfos@hotmail.com>
2021-03-17 17:00:12 +0100

    added ssr support for meterial

--- pages/characters/[pid].js
+++ pages/characters/[pid].js
@@ -1,13 +1,66 @@
 import { gql } from "@apollo/client"
 import client from "../../apollo-client"
 import { useRouter } from "next/router"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
 
 const PeoplePage = ({ character }) => {
+  const useStyles = makeStyles({
+    container: {
+      display: "flex",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  })
+
   const router = useRouter()
   const { pid } = router.query
   console.log("character: ", character)
+  const classes = useStyles()
 
-  return <p>Post: {pid}</p>
+  return (
+    <div className={classes.container}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {character.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
 
 export async function getStaticPaths() {
@@ -47,6 +100,7 @@ export async function getStaticProps({ params }) {
           species
           type
           gender
+          image
           origin {
             name
           }
